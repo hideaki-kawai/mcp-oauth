f35ae1becae69dcb41cd2729d0291e760b343c5d
hideaki.kawai <h.kawai.tech@gmail.com>
2026-04-29 15:48:47 +0900

    fix

--- apps/oauth/vite.config.ts
+++ apps/oauth/vite.config.ts
@@ -1,8 +1,7 @@
 import { cloudflare } from '@cloudflare/vite-plugin'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
-import ssrPlugin from 'vite-ssr-components/plugin'
 
 export default defineConfig({
-  plugins: [cloudflare(), tailwindcss(), ssrPlugin()],
+  plugins: [cloudflare(), tailwindcss()],
 })
